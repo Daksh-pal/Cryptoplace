f8320f53672d42d6c8f1f02b61097c76c2726a25
Daksh Pal <pal.daksh22@gmail.com>
2024-08-08 13:55:28 +0530

    Create Home an Coin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
+import {Route , Routes} from 'react-router-dom'
+import Home from './pages/home/Home'
+import Coin from './pages/coin/Coin'
+
+
+// CG-hDrZbhwFp7sPemkA1Uk4yfYe   <--API KEY
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path='/' element= <Home/> />
+        <Route path='/coin/:coinId' element= <Coin/> />
+      </Routes>
     </div>
   )
 }
